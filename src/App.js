dea00423a3209afa3d7615c0bacc5e830e0409b8
Ahmed Ali <ahmedawan43011@gmail.com>
2022-04-11 14:47:57 +0500

    state time interval

--- src/App.js
+++ src/App.js
@@ -6,12 +6,13 @@ function App() {
   const [number, setNumber] = useState(0);
   return (
     <>
-    <h1>{number}</h1>
-    <button onClick={() => {
-      setNumber(number + 1);
-      setNumber(number + 1);
-      setNumber(number + 1);
-    }}>+3</button>
+      <h1>{number}</h1>
+      <button onClick={() => {
+        setNumber(number + 5);
+        setTimeout(() => {
+          alert(number);
+        }, 3000);
+      }}>+5</button>
     </>
   );
 }
