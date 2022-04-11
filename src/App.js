a5a9bc31c43b52ae629d6ecbbf5c9486618a2320
Ahmed Ali <ahmedawan43011@gmail.com>
2022-04-11 14:27:39 +0500

    state-render

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
 
 import './App.css';
 import { useState } from 'react';
-function sendMessage(message) {
-  // ...
-}
+
 function App() {
-  const [isSent, setIsSent] = useState(false);
-  const [message, setMessage] = useState('Hi');
-  if (isSent) {
-    return <h1>Your message is on its way!</h1>
-  }
+  const [number, setNumber] = useState(0);
   return (
-    <form onSubmit={(e) => {
-      e.preventDefault();
-      setIsSent(true);
-      sendMessage(message);
-    }}>
-      <textarea
-        placeholder="Message"
-        value={message}
-        onChange={e => setMessage(e.target.value)}
-      />
-      <button type="submit">Send</button>
-    </form>
+    <>
+    <h1>{number}</h1>
+    <button onClick={() => {
+      setNumber(number + 1);
+      setNumber(number + 1);
+      setNumber(number + 1);
+    }}>+3</button>
+    </>
   );
 }
 
